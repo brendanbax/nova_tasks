5e1bd06bef817de991de029bbd8e521ee9379993
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-04 16:37:07 -0500

    added example for non class components

--- src/components/NotClassComponent.vue
+++ src/components/NotClassComponent.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
     <h1>Not Class Example</h1>
+    <p>{{ message }}</p>
+    <p>{{ fullName }}</p>
+    <p>{{ count }}</p>
+    <button @click="handleClick">Increment</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  name: 'NotClassComponent',
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      count: 0,
+      first: 'Brendan',
+      last: 'Bax'
+    }
+  },
+  computed: {
+    fullName(): string {
+      return `${this.first} ${this.last}`
+    }
+  },
+  methods: {
+    handleClick(): number {
+      return this.count++
+    }
+  }
+})
 </script>
 
 <style scoped></style>
