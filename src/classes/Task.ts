3abdfd3d776ceda0f30b35b6a60f78f374c165e6
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-04 16:26:58 -0500

    ran prettier SMASH to reformat to my rules

--- src/classes/Task.ts
+++ src/classes/Task.ts
@@ -1,23 +1,23 @@
-import ITask from "../interfaces/Task";
-import { v4 as uuidv4 } from "uuid";
+import ITask from '../interfaces/Task'
+import { v4 as uuidv4 } from 'uuid'
 
 class Task implements ITask {
-  id: string;
-  title: string;
-  body: string;
-  creationDate: Date;
-  dueDate: unknown;
-  tags: Array<string>;
-  status: string;
+  id: string
+  title: string
+  body: string
+  creationDate: Date
+  dueDate: unknown
+  tags: Array<string>
+  status: string
 
   constructor({ title, body, dueDate, status, tags }: ITask) {
-    this.id = uuidv4();
-    this.title = title;
-    this.body = body || "";
-    this.creationDate = new Date();
-    this.dueDate = dueDate || null;
-    this.status = status || "";
-    this.tags = tags || [];
+    this.id = uuidv4()
+    this.title = title
+    this.body = body || ''
+    this.creationDate = new Date()
+    this.dueDate = dueDate || null
+    this.status = status || ''
+    this.tags = tags || []
   }
 
   get info(): ITask {
@@ -28,9 +28,9 @@ class Task implements ITask {
       creationDate: this.creationDate,
       dueDate: this.dueDate,
       status: this.status,
-      tags: this.tags,
-    };
+      tags: this.tags
+    }
   }
 }
 
-export default Task;
+export default Task
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,53 +1,52 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-import New from "../views/New.vue";
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+import New from '../views/New.vue'
 // Samples
-import ClassComponent from "../components/ClassComponent.vue";
-import NotClassComponent from "../components/NotClassComponent.vue";
+import ClassComponent from '../components/ClassComponent.vue'
+import NotClassComponent from '../components/NotClassComponent.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: "/new",
-    name: "NewTask",
-    component: New,
+    path: '/new',
+    name: 'NewTask',
+    component: New
   },
   {
-    path: "/class-example",
-    name: "Class Example",
+    path: '/class-example',
+    name: 'Class Example',
     component: ClassComponent,
     props: {
-      message: "Message from router",
-    },
+      message: 'Message from router'
+    }
   },
   {
-    path: "/not-class-example",
-    name: "Not Class Example",
+    path: '/not-class-example',
+    name: 'Not Class Example',
     component: NotClassComponent,
     props: {
-      message: "Message from router",
-    },
+      message: 'Message from router'
+    }
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
 const router = new VueRouter({
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
