40c9203e0fdf74a3f9b5cbc215f69c258421754f
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-06 13:45:31 -0500

    Moved styling to dedicated files

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import '@/styles/reset.css'
+import '@/styles/vars.css'
 import NavBar from '@/components/NavBar.vue'
 
 @Component({
@@ -18,24 +20,19 @@ export default class extends Vue {}
 </script>
 
 <style>
+body {
+  background: radial-gradient(circle at top, var(--blue-700), var(--blue-900)) no-repeat;
+  min-height: 100vh;
+}
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
