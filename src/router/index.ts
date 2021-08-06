ebf6fc00a85e59ab3c7f540a0923f1a4e2824036
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-05 19:51:58 -0500

    updated routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,42 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+// Views
+import Home from '../views/Home.vue'
+import List from '../views/List.vue'
+import Calendar from '../views/Calendar.vue'
 import New from '../views/New.vue'
-// Samples
-import ClassComponent from '../components/ClassComponent.vue'
-import NotClassComponent from '../components/NotClassComponent.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/new',
     name: 'New',
     component: New
   },
   {
-    path: '/new',
-    name: 'NewTask',
-    component: New
+    path: '/list',
+    name: 'List',
+    component: List
   },
   {
-    path: '/class-example',
-    name: 'Class Example',
-    component: ClassComponent,
-    props: {
-      message: 'Message from router'
-    }
-  },
-  {
-    path: '/not-class-example',
-    name: 'Not Class Example',
-    component: NotClassComponent,
-    props: {
-      message: 'Message from router'
-    }
-  },
-  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
+  }
+  /*{
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    route level code-splitting
+    this generates a separate chunk (about.[hash].js) for this route
+    which is lazy-loaded when the route is visited.
+    component: () => import('../views/About.vue') //* webpackChunkName: "about" 
+  }*/
 ]
 
 const router = new VueRouter({
