a23f3933d6b8cb7f83b518a3cd8c247928fed1ec
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-10 13:30:09 -0500

    added top bar to app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TopBar />
     <router-view />
     <NavBar />
   </div>
@@ -10,11 +11,14 @@ import { Component, Vue } from 'vue-property-decorator'
 import { GET_LOCAL_STATE } from '@/store/actionTypes'
 import '@/styles/reset.css'
 import '@/styles/vars.css'
+import '@/styles/global.css'
 import NavBar from '@/components/NavBar.vue'
+import TopBar from '@/components/TopBar.vue'
 
 @Component({
   components: {
-    NavBar
+    NavBar,
+    TopBar
   },
   mounted() {
     this.$store.dispatch(GET_LOCAL_STATE)
@@ -24,19 +28,12 @@ export default class extends Vue {}
 </script>
 
 <style>
-body {
-  background: radial-gradient(circle at top, var(--blue-700), var(--blue-900)) no-repeat;
-  min-height: 100vh;
-}
 #app {
-  color: #2c3e50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: auto;
 }
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <TaskForm v-if="showNewTask" @cancel="toggleNewTask" />
+  <div v-else class="container">
+    <button @click="toggleNewTask">Add Task</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TaskForm from '@/components/TaskForm.vue'
+
+export default Vue.extend({
+  components: {
+    TaskForm
+  },
+  data() {
+    return {
+      showNewTask: false
+    }
+  },
+  methods: {
+    toggleNewTask(): void {
+      this.showNewTask = !this.showNewTask
+    }
+  }
+})
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: flex-end;
+  margin: 1.5rem 1.5rem 0;
+}
+</style>
