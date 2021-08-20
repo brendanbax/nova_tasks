a02a219716132af765628ca06e18ccd885a815a5
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-20 15:52:28 -0500

    revert app options

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { GET_LOCAL_STATE } from '@/store/actionTypes'
-// import { IBeforeInstall } from '@/interfaces/IBeforeInstall'
+import { IBeforeInstall } from '@/interfaces/IBeforeInstall'
 import '@/styles/reset.css'
 import '@/styles/vars.css'
 import '@/styles/global.css'
@@ -29,38 +29,38 @@ export default Vue.extend({
 
     // Handle PWA prompt
     // Var to store install prompt event
-    // let installPrompt: IBeforeInstall | null
+    let installPrompt: IBeforeInstall | null
 
-    // window.addEventListener('beforeinstallprompt', (e) => {
-    //   // Prevent Chrome and earlier from auto showing the prompt
-    //   e.preventDefault()
-    //   // Stash the event so it can be triggered later
-    //   installPrompt = e
-    //   // Show element in the UI
-    //   this.showInstall = true
-    // })
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome and earlier from auto showing the prompt
+      e.preventDefault()
+      // Stash the event so it can be triggered later
+      installPrompt = e
+      // Show element in the UI
+      this.showInstall = true
+    })
 
-    // this.installer = () => {
-    //   // Hide element
-    //   this.showInstall = false
-    //   // Show prompt
-    //   if (installPrompt) {
-    //     installPrompt.prompt()
-    //     // Wait for user to respond
-    //     // This is a TS thing too... when destructuring an object as a parameter for a function...
-    //     // You will need to define the TYEP of the value... but you can't do { outcome: string } b/c it's treated as a value
-    //     // Instead you can type the destructured object via { key }: { key: type } as per below
-    //     installPrompt.userChoice.then(({ outcome }: { outcome: string }) => {
-    //       if (outcome === 'accepted') {
-    //         console.log('User accepted')
-    //       } else {
-    //         console.log('User denied')
-    //       }
-    //       installPrompt = null
-    //     })
-    //   }
-    //   return null
-    // }
+    this.installer = () => {
+      // Hide element
+      this.showInstall = false
+      // Show prompt
+      if (installPrompt) {
+        installPrompt.prompt()
+        // Wait for user to respond
+        // This is a TS thing too... when destructuring an object as a parameter for a function...
+        // You will need to define the TYEP of the value... but you can't do { outcome: string } b/c it's treated as a value
+        // Instead you can type the destructured object via { key }: { key: type } as per below
+        installPrompt.userChoice.then(({ outcome }: { outcome: string }) => {
+          if (outcome === 'accepted') {
+            console.log('User accepted')
+          } else {
+            console.log('User denied')
+          }
+          installPrompt = null
+        })
+      }
+      return null
+    }
   },
   methods: {
     handleClose() {
