ca42b929d2eeec9547d3aa462d285f3c5b2d474d
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-20 16:45:52 -0500

    deploy

---
+++ precache-manifest.ea61f2208fb4e37570dbef30306a6e19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a04f4e35196b6247fd",
+    "url": "/nova_tasks/css/app.b83aff92.css"
+  },
+  {
+    "revision": "f449c95c254567bafa4d",
+    "url": "/nova_tasks/css/chunk-203bc812.c5f76fe2.css"
+  },
+  {
+    "revision": "ba7b71f2641ac660cfd2",
+    "url": "/nova_tasks/css/chunk-5896672c.cad3171f.css"
+  },
+  {
+    "revision": "8a29644a89d01166e1ae",
+    "url": "/nova_tasks/css/chunk-6eabe409.27642581.css"
+  },
+  {
+    "revision": "4dfe44aa8c0a52659837a806609a017d",
+    "url": "/nova_tasks/index.html"
+  },
+  {
+    "revision": "75a04f4e35196b6247fd",
+    "url": "/nova_tasks/js/app.e708cf75.js"
+  },
+  {
+    "revision": "f449c95c254567bafa4d",
+    "url": "/nova_tasks/js/chunk-203bc812.1d8bc0ea.js"
+  },
+  {
+    "revision": "ba7b71f2641ac660cfd2",
+    "url": "/nova_tasks/js/chunk-5896672c.18cc94fb.js"
+  },
+  {
+    "revision": "8a29644a89d01166e1ae",
+    "url": "/nova_tasks/js/chunk-6eabe409.5b6113e2.js"
+  },
+  {
+    "revision": "9b044f1af9dbadc73137",
+    "url": "/nova_tasks/js/chunk-vendors.3db8dfec.js"
+  },
+  {
+    "revision": "b89992a65b3ec3c88b592dfa031d091d",
+    "url": "/nova_tasks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nova_tasks/robots.txt"
+  }
+]);
