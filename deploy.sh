85a8c12b4ed80603a16675ca5121a66aebc2dcf4
Brendan Bax <brendan.bax.ext@bayer.com>
2021-08-20 15:37:29 -0500

    DEBUG: attempted fix for deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:brendanbax/task_master.git master:gh-pages
+git push -f git@github.com:brendanbax/nova_tasks.git master:gh-pages
 cd -
